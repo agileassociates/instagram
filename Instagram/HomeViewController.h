c41cdeb42ecd9a0b9667fae4a63a7c6a4d7c42ec
Johny Babylon <Home@johnys-macbook.attlocal.net>
2016-08-28 12:12:44 -0700

    added Facebook login

---
+++ Instagram/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+HomeViewController.h
+//  Instagram
+//
+//  Created by Johny Babylon on 8/26/16.
+//  Copyright © 2016 Johny Babylon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ Instagram/HomeViewController.m
@@ -0,0 +1,45 @@
+//
+//  UIViewController+HomeViewController.m
+//  Instagram
+//
+//  Created by Johny Babylon on 8/26/16.
+//  Copyright © 2016 Johny Babylon. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+    
+    
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    
+    if ([FBSDKAccessToken currentAccessToken]) {
+        // User is logged in, do work such as go to next view controller.
+        [self performSegueWithIdentifier:@"facebook_login" sender:self];
+        
+    }
+    
+    
+
+    
+}
+
+
+
+
+@end
